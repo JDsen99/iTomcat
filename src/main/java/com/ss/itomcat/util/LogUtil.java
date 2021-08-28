c9b8e0ee23e648cb3f2797cfa7da5f7d1f3afd50
JDsen99 <64098056@qq.com>
2021-08-28 16:18:32 +0800

    更新日志工具类

---
+++ src/main/java/com/ss/itomcat/util/LogUtil.java
@@ -0,0 +1,16 @@
+package com.ss.itomcat.util;
+
+import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author JDsen99
+ * @description
+ * @createDate 2021/8/28-15:56
+ */
+public class LogUtil {
+    public static Logger getLogger() {
+        return LoggerFactory.getLogger("test");
+    }
+}
