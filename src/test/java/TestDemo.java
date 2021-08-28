9f2433a8b9a146e47c7ea03cc39f7acce9d73ec6
JDsen99 <64098056@qq.com>
2021-08-28 16:18:39 +0800

    测试类

---
+++ src/test/java/TestDemo.java
@@ -0,0 +1,14 @@
+import com.ss.itomcat.util.LogUtil;
+import org.slf4j.Logger;
+
+/**
+ * @author JDsen99
+ * @description
+ * @createDate 2021/8/28-16:05
+ */
+public class TestDemo {
+    public static void main(String[] args) {
+        Logger logger = LogUtil.getLogger();
+        logger.info("111");
+    }
+}
