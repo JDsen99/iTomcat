1ef58ad333505fbfa99b59d91df26e190de7ce93
JDsen99 <64098056@qq.com>
2021-08-28 15:51:03 +0800

    更新启动类

---
+++ src/main/java/com/ss/itomcat/startuo/BootStrap.java
@@ -0,0 +1,13 @@
+package com.ss.itomcat.startuo;
+
+/**
+ * @author JDsen99
+ * @description
+ * @createDate 2021/8/28-15:40
+ */
+public class BootStrap {
+
+    public static void main(String[] args) {
+
+    }
+}
