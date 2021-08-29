db0975b0a9aa844132f1242bb67af3031dbef75c
JDsen99 <64098056@qq.com>
2021-08-29 16:44:58 +0800

    更新Catalina

--- src/main/java/com/ss/itomcat/startup/Catalina.java
+++ src/main/java/com/ss/itomcat/startup/Catalina.java
@@ -1,6 +1,13 @@
 package com.ss.itomcat.startup;
 
 import com.ss.itomcat.catalina.Server;
+import com.ss.itomcat.util.Digester;
+import org.dom4j.DocumentException;
+
+import javax.annotation.processing.Filer;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 /**
  * @author JDsen99
@@ -12,11 +19,47 @@ public class Catalina {
     /**
      * 配置文件地址
      */
-    protected static final String confPath = "conf/server.xml";
+    protected static final String configFile = "conf/server.xml";
 
     protected Server server = null;
 
-    private void load() {
 
+
+    private void load() {
+        try {
+            Digester digester = createStartDigester();
+            File file = configFile();
+            FileInputStream inputStream = new FileInputStream(file);
+            digester.push(this);
+            digester.parse(inputStream);
+        } catch (DocumentException | FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        getServer().init();
+    }
+
+    protected File configFile() {
+        String catalinaBase = this.getClass()
+                .getResource("/")
+                .getPath()
+                .replace("/target/classes","") + "source/";
+
+        File file = new File(catalinaBase);
+        if (!file.isAbsolute()) {
+            file = new File(catalinaBase,configFile);
+        }
+        return file;
+    }
+
+    private Digester createStartDigester() {
+        return new Digester();
+    }
+    public Server getServer() {
+        return server;
+    }
+
+    public void setServer(Server server) {
+        this.server = server;
     }
 }
